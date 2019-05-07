a9d3c1a440c2025247419d9601141cad96e6e3d0
chrisvaillancourt <44307925+chrisvaillancourt@users.noreply.github.com>
2019-05-07 10:20:02 -0700

    added strict mode and window.onload

--- js/index.js
+++ js/index.js
@@ -1,51 +1,59 @@
-require([
-  "esri/Map",
-  "esri/views/MapView",
-  "esri/layers/FeatureLayer",
-  "esri/widgets/LayerList"
-],
-  function (Map, MapView, FeatureLayer, LayerList) {
-    let map = new Map({
-      basemap: "gray-vector"
-    });
-    let view = new MapView({
-      container: "viewDiv",
-      map: map,
-      zoom: 4,
-      center: [-97, 39] // longitude, latitude
-    });
-    let counties = new FeatureLayer({
-      portalItem: {
-        id: "7566e0221e5646f99ea249a197116605"
-      },
-      visible: true,
-      id: "counties"
-    });
-    let states = new FeatureLayer({
-      portalItem: {
-        id: "99fd67933e754a1181cc755146be21ca"
-      },
-      visible: false,
-      id: "states"
-    });
-    let layerList = new LayerList({
-      view: view
-    });
+"use strict"
 
-    // add layers to map
-    map.addMany([states, counties]);
+window.onload = event => {
+  map();
+};
 
-    // add layerList widget to the top right of the UI
-    view.ui.add(layerList, "top-right");
-    counties.watch("visible", function (visible) {
-      if (visible) {
-        states.visible = false;
-      }
-    })
-    states.watch("visible", function (visible) {
-      if (visible) {
-        counties.visible = false;
-      }
-    })
+function map() {
+  require([
+    "esri/Map",
+    "esri/views/MapView",
+    "esri/layers/FeatureLayer",
+    "esri/widgets/LayerList"
+  ],
+    function (Map, MapView, FeatureLayer, LayerList) {
+      let map = new Map({
+        basemap: "gray-vector"
+      });
+      let view = new MapView({
+        container: "viewDiv",
+        map: map,
+        zoom: 4,
+        center: [-97, 39] // longitude, latitude
+      });
+      let counties = new FeatureLayer({
+        portalItem: {
+          id: "7566e0221e5646f99ea249a197116605"
+        },
+        visible: true,
+        id: "counties"
+      });
+      let states = new FeatureLayer({
+        portalItem: {
+          id: "99fd67933e754a1181cc755146be21ca"
+        },
+        visible: false,
+        id: "states"
+      });
+      let layerList = new LayerList({
+        view: view
+      });
 
-  });
+      // add layers to map
+      map.addMany([states, counties]);
+
+      // add layerList widget to the top right of the UI
+      view.ui.add(layerList, "top-right");
+      counties.watch("visible", function (visible) {
+        if (visible) {
+          states.visible = false;
+        }
+      });
+      states.watch("visible", function (visible) {
+        if (visible) {
+          counties.visible = false;
+        }
+      });
+
+    });
+};
